e9ec983b2903f120851d342943a954972da102d7
me <regina.chow3144@gmail.com>
2019-02-09 16:15:43 -0500

    added icons, added class

---
+++ StreetSmart/ProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewController.swift
+//  StreetSmart
+//
+//  Created by Regina Chau on 2/9/19.
+//  Copyright © 2019 Vicky Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(red: 57/255, green: 77/255, blue:254/255, alpha: 1)
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+}
