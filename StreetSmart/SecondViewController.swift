364627cef7933ddb4b507613b8f09df6257715da
Vicky Zheng <vickyzheng205@gmail.com>
2019-02-09 13:15:52 -0500

    Initial Commit

---
+++ StreetSmart/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  StreetSmart
+//
+//  Created by Vicky Zheng on 2/9/19.
+//  Copyright © 2019 Vicky Zheng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
